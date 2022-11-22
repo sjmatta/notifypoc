117640bef77d0593c4e3e4734575f06550154469
Stephen Matta <stephenjmatta@gmail.com>
2022-11-22 15:47:48 -0500

    Initial commit

---
+++ src/main/java/com/stephenmatta/notifypoc/NotifypocApplication.java
@@ -0,0 +1,13 @@
+package com.stephenmatta.notifypoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifypocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifypocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stephenmatta/notifypoc/NotifypocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stephenmatta.notifypoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifypocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
